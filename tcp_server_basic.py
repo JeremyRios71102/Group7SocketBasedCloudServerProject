abde29916246dd1d9eaf1d1f594f775b05e78aaa
Paul-p03 <168858558+Paul-p03@users.noreply.github.com>
2024-11-19 13:33:51 -0500

    Update tcp_server_basic.py
    Correct version of server code

--- tcp_server_basic.py
+++ tcp_server_basic.py
@@ -110,34 +110,3 @@ def start_server():
 
 if __name__ == '__main__':
     start_server()
-
-# tcp_server_basic.py
-#!/usr/bin/env python3
-import socket
-
-# host binds to local server ip
-host = '10.128.0.2'
-port = 3300
-BUFFER_SIZE = 1024
-dashes = '----> '
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_tcp:
-    server_tcp.bind((host, port))
-    # wait for client connection
-    while True:
-        server_tcp.listen(6)
-        print('[*] Waiting for connection')
-        # establish client connection
-        connection, addr = server_tcp.accept()
-        with connection:
-            print(f'[*] Established connection from IP {addr[0]} port: {addr[1]}')
-            while True:
-                # receive bytes
-                data = connection.recv(BUFFER_SIZE)
-                # verify received data
-                if not data:
-                    break
-                else:
-                    # convert to string
-                    print('[*] Data received: {}'.format(data.decode('utf-8')))
-                    connection.send(dashes.encode('utf-8') + data)  # echo data back to origin
